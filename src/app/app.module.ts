1a048d209dca42ee3290acc5eaee168a7b2e3c77
Karthik C <ckarthikmuthu1234@gmail.com>
2022-04-27 14:59:39 +0530

    Updated the center component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './header/header.component';
+import { CenterComponent } from './center/center.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    CenterComponent,
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
